52fda3948736c055e720fe4b4e6c22f22338c91b
KamiZan712 <jrydggtnsndjsldnjlagt@gmail.com>
2025-05-29 21:54:12 +0900

    キャッシュを能動的に消す機能を追加v3

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const cacheName = "dice-game-v2";
+const cacheName = "dice-game-v3";
 const filesToCache = [
   "./",
   "./index.html",
@@ -20,6 +20,22 @@ self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
+    })
+  );
+});
+
+//古いキャッシュを能動的に消す
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [cacheName];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((name) => {
+          if (!cacheWhitelist.includes(name)) {
+            return caches.delete(name);
+          }
+        })
+      );
     })
   );
 });
